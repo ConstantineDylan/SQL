990c0377748f24fec2537cd6aa8969e6d4ea7b9f
ConstantineDylan <dylansugito@gmail.com>
2021-10-15 15:59:40 +0700

    updated query with new table and joins

--- Test Query.sql
+++ Test Query.sql
@@ -21,23 +21,81 @@ VALUES
 
 --@block
 -- SELECT * FROM users;
-SELECT email, id FROM Users
+SELECT id, email FROM users
 
 WHERE country = 'us'
-AND id > 1
+AND id = 1
 AND email LIKE 'h%'
 
 ORDER BY id ASC
-LIMIT 2;
 
 
 --@block
+--faster read yet slower write 
+-- at the cost of additional memory
+CREATE INDEX email_index ON users(email);
+
+--@block
+CREATE TABLE Rooms(
+    id INT AUTO_INCREMENT,
+    street VARCHAR(255),
+    owner_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (owner_id) REFERENCES users(id)
+)
 
 
 --@block
+DROP TABLE rooms
 
 
 --@block
+INSERT INTO rooms(owner_id, street)
+VALUES 
+    (1, 'san diego sailboat'),
+    (1, 'nantucket cottage'),
+    (1, 'vail cabin'),
+    (1, 'sf cardboard box');
 
 
+--@block
+SELECT 
+    users.id AS user_id,
+    rooms.id AS room_id,
+    email,
+    street
+FROM users 
+INNER JOIN rooms
+ON rooms.owner_id = users.id;
 
+
+--@block
+CREATE TABLE Bookings(
+    id INT AUTO_INCREMENT,
+    guest_id INT NOT NULL,
+    room_id INT NOT NULL,
+    check_in DATETIME,
+    PRIMARY KEY(id),
+    FOREIGN KEY (guest_id) REFERENCES users(id),
+    FOREIGN KEY (room_id) REFERENCES rooms(id)
+);
+
+
+--@block Rooms a user has Booked
+SELECT 
+    guest_id,
+    street,
+    check_in
+FROM bookings
+INNER JOIN rooms on rooms.owner_id = guest_id
+WHERE guest_id = 1;
+
+
+--@block
+SELECT 
+    email,
+    guest_id,
+    check_in,
+    room_id
+FROM bookings INNER JOIN users 
+ON users.id = guest_id;
